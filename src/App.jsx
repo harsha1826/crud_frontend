3f4019227e5a949a4e6adcfa32115daf19d90bef
harsha1826 <2300031826@kluniversity.in>
2025-08-19 11:54:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:9091';
+const BASE_URL = 'http://localhost:9090';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
